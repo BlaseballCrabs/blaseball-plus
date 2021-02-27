500c094ecb4f686d8c6c724d250dd6f0cccba89d
leo60228 <leo@60228.dev>
2021-02-27 14:44:11 -0500

    peanut attributes

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -36,6 +36,10 @@ const config = env => ({
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.svg$/i,
+                type: 'asset/source'
             }
         ]
     }
